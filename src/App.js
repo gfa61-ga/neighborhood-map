05dca113676e6274abb90441efce2329a9483a9f
George Al <gfa61.ga@gmail.com>
2018-06-25 01:17:30 +0300

    feat: Add mockup HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="input-area">
+          <div className="search-places">
+            <select id="place-filter">
+              <option value="">Filter Places by</option>
+              <option value="food">Food</option>
+              <option value="shops">Shops</option>
+              <option value="fun">Fun</option>
+            </select>
+          </div>
+          <div className="neighborhhood-location">
+            <input id="location-input" type="text" placeholder="Athens, Greece"/>
+            <input id="location-button" type="button" value="Go"/>
+            <input id="hide-list-button" type="button" value="&#9776;"/>
+          </div>
+        </div>
+
+        <div className="place-list">
+          <ul>
+            <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li>
+            <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li>
+            <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li>
+            <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li>
+            <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li>
+            <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li>
+            <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li>
+            <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li>
+            <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li> <li>Place 1</li>
+          </ul>
+        </div>
+
+        <div className="list-footer">
+        </div>
+
+        <div className="map-area">
+          <div id="map"/>
+        </div>
       </div>
     );
   }
