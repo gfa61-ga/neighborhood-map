03f422df6ea4f8d9b1e9098cd2532fb85f0cacf1
George Al <gfa61.ga@gmail.com>
2018-07-09 00:53:11 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,47 @@
-"use strict";var precacheConfig=[["/neighborhood-map/index.html","9714b8b5bbab0c369c2cd60100b6f7e1"],["/neighborhood-map/static/css/main.656f58b8.css","8505d22bd2d70672b7b3be51d42eab3c"],["/neighborhood-map/static/js/main.b52933ad.js","b37bbeecb0656e303ac010b1200bc741"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,/\.\w{8}\./);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(r){return setOfCachedUrls(r).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return r.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),r="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,r),e=urlsToCacheKeys.has(n));var a="/neighborhood-map/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/* Create 'neighborhood-map' cache if there isn't one
+ * or get 'rneighborhood-map' cache object
+ * and store it in currentCache property of service-worker
+*/
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('neighborhood-map').then(cache => {
+      self.currentCache = cache;
+    })
+  );
+});
+
+// Add event listener for 'fetch' events
+self.addEventListener('fetch', (event) => {
+
+  /* When a 'fetch' event occurs:
+   *
+   * 1. Take control of fetch response
+   */
+  event.respondWith(
+    // Search if the response is stored in cache.
+    caches.match(event.request).then(cacheResponse => {
+      if (cacheResponse) {
+        // If response is found in cache, send it to the browser
+        return cacheResponse;
+      } else {
+        /* If response is not found in cache,
+         * fetch it from the network
+         * and then send it to the browser
+         */
+        return fetch(event.request);
+      }
+    })
+  );
+
+  /*
+   * 2. If response is not saved in cache
+   */
+  caches.match(event.request).then(cacheResponse => {
+    if (!cacheResponse) {
+      // Fetch it again from the network and then save it in cache
+      fetch(event.request).then(netResponse => {
+        self.currentCache.put(event.request, netResponse);
+      });
+    }
+  });
+});
